60e4a44b4614bea17c5c3feeacb6f47ba5d781a6
davidthakur <vr384695@gmail.com>
2024-02-18 23:33:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components';
 import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data.js';
-
+import {toast} from 'react-toastify'
 const App = () => {
   return (
    <>
